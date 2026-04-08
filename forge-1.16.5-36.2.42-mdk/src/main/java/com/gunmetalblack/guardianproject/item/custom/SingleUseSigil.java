0c46a8fd6d1dcd287bfdb86c1aa66d3a5decba2d
TheJuicedOrange <powerhousegames206@gmail.com>
2026-04-08 01:18:30 -0400

    Working Sigil Prototype Complete

--- forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/custom/SingleUseSigil.java
+++ forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/custom/SingleUseSigil.java
@@ -1,5 +1,6 @@
 package com.gunmetalblack.guardianproject.item.custom;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,9 +9,7 @@ import net.minecraft.item.UseAction;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
 
 public class SingleUseSigil extends Item {
@@ -18,32 +17,62 @@ public class SingleUseSigil extends Item {
         super(properties);
     }
 
+    // 1. This starts the "charging" process
     @Override
-    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
-        World world = context.getLevel();
-        if(!world.isClientSide())
-        {
-            PlayerEntity player = context.getPlayer();
-            onSigilUse(player, world);
-            stack.shrink(1);
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        player.startUsingItem(hand);
+        return ActionResult.consume(itemstack);
+    }
+
+    // 2. This triggers when the player lets go
+    @Override
+    public void releaseUsing(ItemStack stack, World world, LivingEntity entity, int timeLeft) {
+        if (entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            int durationHeld = this.getUseDuration(stack) - timeLeft;
+
+            // Calculate power (0.0 to 1.0)
+            float power = getPowerForTime(durationHeld);
+
+            // Only trigger if there is meaningful power (e.g., held for at least a few ticks)
+            if (power > 0.1F) {
+                if (!world.isClientSide()) {
+                    // Pass the power to the effect logic
+                    onSigilUse(player, world, power);
+
+                    if (!player.abilities.instabuild) {
+                        stack.shrink(1);
+                    }
+                }
+            }
         }
-        return super.onItemUseFirst(stack, context);
     }
 
     @Override
     public int getUseDuration(ItemStack stack) {
-        return 72000; // Standard for items that can be held indefinitely
+        return 72000;
     }
-
     @Override
     public UseAction getUseAnimation(ItemStack stack) {
-        return UseAction.BOW; // This makes the player's arm pull back like a bow
+        return UseAction.BOW;
+    }
+    public static float getPowerForTime(int ticks) {
+        float f = (float)ticks / 20.0F; // Full power reached at 1 second (20 ticks)
+        f = (f * f + f * 2.0F) / 3.0F;
+        return Math.min(f, 10.0F);
     }
 
-    public static void onSigilUse(PlayerEntity player, World world)
-    {
+    public static void onSigilUse(PlayerEntity player, World world, float power) {
         world.playSound(null, player.getX(), player.getY(), player.getZ(),
-                SoundEvents.AMBIENT_NETHER_WASTES_MOOD, SoundCategory.PLAYERS, 1.0F, 1.0F);
-        player.addEffect(new EffectInstance(Effects.ABSORPTION, 100, 2));
+                SoundEvents.AMBIENT_NETHER_WASTES_MOOD, SoundCategory.PLAYERS, 1.0F, 0.5F + (power * 0.5F));
+
+        // Scale duration: 200 ticks (10s) at max power, down to 0 at no power
+        int baseDuration = 200;
+        int scaledDuration = (int)(baseDuration * power);
+
+        if (scaledDuration > 0) {
+            player.addEffect(new EffectInstance(Effects.ABSORPTION, scaledDuration, 2));
+        }
     }
 }
