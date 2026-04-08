196e26be43454b9e78e78252daa11a74bfab7497
TheJuicedOrange <powerhousegames206@gmail.com>
2026-04-08 01:04:55 -0400

    Working

--- forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/ModItems.java
+++ forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.gunmetalblack.guardianproject.item;
 
 import com.gunmetalblack.guardianproject.GuardianProjectMod;
+import com.gunmetalblack.guardianproject.item.custom.SingleUseSigil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,7 +15,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, GuardianProjectMod.MOD_ID);
 
     public static final RegistryObject<Item> BLANK_SIGIL = ITEMS.register("blank_sigil",
-            ()-> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+            ()-> new SingleUseSigil(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
 
     public static void register(IEventBus eventBus) {ITEMS.register(eventBus);}
 }
--- forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/custom/SingleUseSigil.java
+++ forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/custom/SingleUseSigil.java
@@ -1,4 +1,49 @@
 package com.gunmetalblack.guardianproject.item.custom;
 
-public class SingleUseSigil {
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.UseAction;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.world.World;
+
+public class SingleUseSigil extends Item {
+    public SingleUseSigil(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
+        World world = context.getLevel();
+        if(!world.isClientSide())
+        {
+            PlayerEntity player = context.getPlayer();
+            onSigilUse(player, world);
+            stack.shrink(1);
+        }
+        return super.onItemUseFirst(stack, context);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack) {
+        return 72000; // Standard for items that can be held indefinitely
+    }
+
+    @Override
+    public UseAction getUseAnimation(ItemStack stack) {
+        return UseAction.BOW; // This makes the player's arm pull back like a bow
+    }
+
+    public static void onSigilUse(PlayerEntity player, World world)
+    {
+        world.playSound(null, player.getX(), player.getY(), player.getZ(),
+                SoundEvents.AMBIENT_NETHER_WASTES_MOOD, SoundCategory.PLAYERS, 1.0F, 1.0F);
+        player.addEffect(new EffectInstance(Effects.ABSORPTION, 100, 2));
+    }
 }
