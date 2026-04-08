9cec1e69bb6bd434a53b89e135271dda71c4d813
TheJuicedOrange <powerhousegames206@gmail.com>
2026-04-07 22:00:53 -0400

    Init

---
+++ forge-1.16.5-36.2.42-mdk/src/main/java/com/gunmetalblack/guardianproject/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.gunmetalblack.guardianproject.item;
+
+import com.gunmetalblack.guardianproject.GuardianProjectMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, GuardianProjectMod.MOD_ID);
+
+    public static final RegistryObject<Item> BLANK_SIGIL = ITEMS.register("blank_sigil",
+            ()-> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+
+    public static void register(IEventBus eventBus) {ITEMS.register(eventBus);}
+}
